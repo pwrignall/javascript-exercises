3d991b6c1e33c27e073b070b2ff97527b737c6b3
Paul Rignall <paul.rignall@gmail.com>
2020-06-04 14:12:23 +0100

    Contains solution for sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
 
+    if (typeof a == 'number' && typeof b == 'number' &&
+        a >= 0 && b >= 0) {
+        let m;
+        let n;
+        if (a < b) {
+            m = a;
+            n = b;
+        } else {
+            m = b;
+            n = a;
+        }
+        let sum = m + n;
+        for (let i = m + 1; i < n; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 }
 
 module.exports = sumAll
