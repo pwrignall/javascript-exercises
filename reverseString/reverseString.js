0cbcc8e14890ab503989cd784e3b14023e9df04f
Paul Rignall <paul.rignall@gmail.com>
2020-06-04 09:50:36 +0100

    Contains solution for reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let originalString = string;
+    let stringArray = string.split('');
+    let reversedArray = stringArray.reverse();
+    return reversedArray.join('');
 }
 
 module.exports = reverseString
