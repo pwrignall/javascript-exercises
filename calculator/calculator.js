afb506f92e1f4c680391c290295d7ae6fdc17d31
Paul Rignall <paul.rignall@gmail.com>
2020-06-10 12:25:09 +0100

    My solution for the calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -12,16 +12,26 @@ function sum(array) {
 	}, 0)
 }
 
-function multiply() {
-
+function multiply(array) {
+	return array.reduce((total, current) => {
+		return total * current;
+	})
 }
 
-function power() {
-
+function power(a, b) {
+	return a ** b;
 }
 
-function factorial() {
-
+function factorial(a) {
+	if (a == 0) {
+		return 1;
+	} else {
+		let runningtotal = a;
+		for (i = a - 1; i > 0; i--) {
+			runningtotal *= i;
+		}
+		return runningtotal;
+	};
 }
 
 module.exports = {
