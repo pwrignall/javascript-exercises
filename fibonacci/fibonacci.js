d8f86d68e4257c1545335ca8f580f137f500343f
Paul Rignall <paul.rignall@gmail.com>
2020-06-10 23:13:44 +0100

    Solution for fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    // const phi = ((1 + Math.sqrt(5)) / 2)
+    // if (n >= 0) {
+    //     return Math.floor((phi ** n / Math.sqrt(5)) + (1 / 2));
+    // } else {
+    //     return "OOPS";
+    // }
 
+    if (n < 0) return 'OOPS';
+    if (n == 0) return 0;
+    let i = 1;
+    let a = 0;
+    let b = 1;
+    let fib;
+    do {
+        fib = a + b;
+        a = b;
+        b = fib;
+        i++;
+    } while (i < n)
+    return fib;
 }
 
 module.exports = fibonacci
