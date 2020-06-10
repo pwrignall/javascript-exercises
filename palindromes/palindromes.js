dc4aac04ac50c0bc3c30f9c725d74c50d78e1e18
Paul Rignall <paul.rignall@gmail.com>
2020-06-10 13:17:57 +0100

    Solution for palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
+const palindromes = function (string) {
+    stringArray = [...string.trim()
+        .toLowerCase()];
 
+    const lettersRegExp = /[a-z]/;
+    const charArray = stringArray.filter(letter => lettersRegExp.test(letter));
+    let revCharArray = [...charArray];
+    revCharArray.reverse();
+
+    return charArray.join('') === revCharArray.join('');
 }
 
 module.exports = palindromes
