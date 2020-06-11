615eca2cb82788e7a433b2a127c86d2d25961145
Paul Rignall <paul.rignall@gmail.com>
2020-06-11 10:46:41 +0100

    Solution for findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
-
+let findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
+    people.map(person => {
+        if (person.yearOfDeath) {
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person['age'] = currentYear - person.yearOfBirth;
+        }
+    });
+    const sortedPeople = people.sort((a, b) => a.age > b.age ? -1 : 1);
+    return sortedPeople[0];
 }
 
 module.exports = findTheOldest
